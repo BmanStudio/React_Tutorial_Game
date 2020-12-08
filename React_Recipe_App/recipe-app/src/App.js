985916ab60776e8751490c0db7c3e9faf261e7d5
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2020-12-08 23:55:22 +0200

    added some more features by the video

--- React_Recipe_App/recipe-app/src/App.js
+++ React_Recipe_App/recipe-app/src/App.js
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from "react";
+import Recipe from "./Recipe";
 import './App.css';
 
 const App = () => {
@@ -6,24 +7,54 @@ const App = () => {
     const APP_ID = '1f1d9f38';
     const APP_KEY = '442ea7ddbf219f65548344c5856a0ee0';
 
-    useEffect(() =>{
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState('');
 
-    }, []);
+    useEffect(() =>{
+        getRecipes();
+        console.log("UseEffect has run")
+    }, [query]);
 
     const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const response = await fetch(
+            `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
         const data = await response.json();
-        console.log(data);
+        setRecipes(data.hits);
     };
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+    }
+
+    const getSearch = e => {
+        console.log(search);
+        e.preventDefault();
+        setQuery(search);
+    }
 
     return (
         <div className={"App"}>
-           <form className={"search-form"}>
-               <input className={"search-bar"} type={"text"}/>
+           <form className={"search-form"} onSubmit={getSearch}>
+               <input
+                   className={"search-bar"}
+                   type={"text"}
+                   onChange={updateSearch}
+                   value={search}
+               />
                <button className={"search-button"} type={"submit"}>
                    Search
                </button>
            </form>
+            {recipes.map(myRecipe => (
+                <Recipe
+                    key={myRecipe.recipe.label}
+                    title={myRecipe.recipe.label}
+                    calories={myRecipe.recipe.calories}
+                    image={myRecipe.recipe.image}
+                />
+            ))}
         </div>
     );
 }
